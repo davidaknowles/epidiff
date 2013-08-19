5e48e3a3061633334077664c031d326bb6d06693
davidaknowles <knowles84@gmail.com>
2013-08-19 12:50:46 -0700

    differential

---
+++ run_kde.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# the next line is a "magic" comment that tells codine to use bash
+#$ -S /bin/bash
+#
+# and now for some real work
+#cd $PBS_O_WORKDIR
+pwd
+echo $SGE_TASK_ID
+python2.6 run_kde.py $SGE_TASK_ID
